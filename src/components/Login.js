b36a5c944b62f87b5f12abadfbbd850956d55d7a
Monly44 <manusic44@gmail.com>
2023-08-10 21:03:28 +0200

    Better

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Style/Login.css";
-import imageSource from "./Style/login.png"; // Replace with your image source
+import imageSource from "./Style/login.png";
 
 const Login = (props) => {
   const [usernameText, setUserNameText] = useState("");
