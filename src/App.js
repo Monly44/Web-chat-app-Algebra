56c214c8dc1499c817f98035fc824411ad461696
Monly44 <133984970+Monly44@users.noreply.github.com>
2023-08-16 17:54:42 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import Chat from "./components/Chat";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import "./components/Style/Style.css"; // Import your main CSS file here
-import { ThemeProvider } from "styled-components";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      username: "",
-      themeColor: "#4802fa", // Default theme color
-    };
+    this.state = { username: "" };
   }
 
   handleLogin = (username) => {
@@ -22,38 +18,18 @@ class App extends React.Component {
     this.setState({ username: "" });
   };
 
-  handleThemeChange = (color) => {
-    this.setState({ themeColor: color });
-  };
-
   render() {
-    const { username, themeColor } = this.state;
-
-    // Define the theme dynamically
-    const theme = {
-      primaryColor: themeColor,
-    };
+    const { username } = this.state;
 
     return (
-      <ThemeProvider theme={theme}>
-        <div
-          className="app-container"
-          style={{
-            background: `linear-gradient(45deg, ${themeColor}a4, ${themeColor}85, ${themeColor}75, ${themeColor}7a, ${themeColor}7b, ${themeColor}a1)`,
-          }}
-        >
-          <Header
-            username={username}
-            handleLogout={this.handleLogout}
-            handleThemeChange={this.handleThemeChange}
-          />
-          {username ? (
-            <Chat username={username} />
-          ) : (
-            <Login onLogin={this.handleLogin} username={username} />
-          )}
-        </div>
-      </ThemeProvider>
+      <div className="app-container">
+        <Header username={username} handleLogout={this.handleLogout} />
+        {username ? (
+          <Chat username={username} />
+        ) : (
+          <Login onLogin={this.handleLogin} username={username} />
+        )}
+      </div>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,17 @@
-import React from "react";
-import styled from "./Style/Header.css"; // Import styled-components
-
-const StyledHeader = styled.div`
-  color: ${(props) => props.theme.primaryColor}; /* Use theme primaryColor */
-  /* ... other styles ... */
-`;
+import "./Style/Header.css";
 
 const Header = (props) => {
   const isLoggedIn = !!props.username;
-
   return (
-    <StyledHeader>
+    <>
+      {}
       {isLoggedIn && (
         <div className="App-header">
-          <h1>Welcome {props.username}</h1>
-          <div className="dropdown">
-            <button className="dropbtn">Menu</button>
-            <div className="dropdown-content">
-              <button onClick={props.handleLogout}>Logout</button>
-              <div>
-                <button onClick={() => props.handleThemeChange("#4802fa")}>
-                  Blue Theme
-                </button>
-                <button onClick={() => props.handleThemeChange("#f44336")}>
-                  Red Theme
-                </button>
-                {/* Add more theme options as needed */}
-              </div>
-            </div>
-          </div>
+          <h1>{props.username}'s Chat Room</h1>
+          <button onClick={props.handleLogout}>Logout</button>
         </div>
       )}
-    </StyledHeader>
+    </>
   );
 };
-
 export default Header;
